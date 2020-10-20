b9a202cb58ee76dca1341cf60dd7361e252884fb
YatesyTea <danyates1997@gmail.com>
2020-10-20 16:00:52 +0100

    Pracital 1 Part 2

--- Dan/Practicals/src/pracitcal1_part2/Histogram.java
+++ Dan/Practicals/src/pracitcal1_part2/Histogram.java
@@ -5,12 +5,48 @@
  */
 package pracitcal1_part2;
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+//import java.math.RoundingMode;
+
 /**
  *
  * @author Yates
  */
 public class Histogram {
-    
-    
-    public static void main (String[] args){}
+
+    public static void main(String[] args) {
+        System.out.print("### Grades Histogram ###");
+        try {
+            File grades = new File("grades.txt");
+            Scanner sc = new Scanner(grades);
+
+            
+            int numGradeBoundaries = 10;
+            int gradesList[] = new int[numGradeBoundaries];
+            for (int j=0; j<numGradeBoundaries; j++){
+                gradesList[j] = 0;
+            }
+            
+            int currentInt, newInt;
+            for (int i = 0; i < 100; i++) {
+                currentInt = sc.nextInt();
+                newInt = (int) Math.ceil((double)currentInt/10);
+                gradesList[newInt - 1] += 1;
+            }
+            
+            for (int m = 0; m < numGradeBoundaries; m++){
+                System.out.print("\n");
+                System.out.print(m+1 + " - " + (m+1)*10 + "  \t|");
+                for (int j=0; j < gradesList[m]; j++){
+                    System.out.print("*");
+                }
+            }
+            System.out.print("\n");
+            
+        } catch (FileNotFoundException e) {
+            System.err.format("File does not exist\n");
+        }
+    }
 }
--- Dan/Practicals/src/practical1_part1/Driver.java
+++ Dan/Practicals/src/practical1_part1/Driver.java
@@ -15,7 +15,7 @@ public class Driver {
      * @param args the command line arguments
      * 
      */
-    public static void main(String[] args) {     
+    public static void main(String[] args) {    
         try {
             // Taking input from the command line.
             Scanner sc = new Scanner(System.in);
